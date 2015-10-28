62a2bfef776e37d36c3795bf4088781f659d2c03
Samuel Woodbridge <s-bridge@live.co.uk>
2015-10-28 10:58:24 +0000

    [Livebookings] - Added in module

---
+++ book-a-table.php
@@ -0,0 +1,31 @@
+<div class="book">
+
+    <div class="centre-wrap centre-wrap--centred centre-wrap--small centre-wrap--no">
+    
+        <div class="generic-form__wrapper">
+            
+            <a href="javscript:void(0);" class="anchor" id="booking"></a>
+            
+            <h3><?= Arr::get($config, 'title');?></h3>
+            <p><?= Arr::get($config, 'subtitle');?></p>
+            
+            <script type="text/javascript" src="https://bda.bookatable.com/deploy/lbui.direct.min.js"></script>
+            <script type="text/javascript">
+
+                LBDirect_Embed({
+
+                    connectionid : "<?= Arr::get($config, 'console');?>",
+                    style : {
+
+                        baseColor  :  "<?= Arr::get($config, 'color', "#000000");?>"
+
+                    }
+                });
+
+            </script>
+            
+        </div>
+    
+    </div>
+
+</div>
