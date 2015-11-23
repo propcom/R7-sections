a9f83dbc7fc33443dce3addc2526f79efc7fb46a
Ritchie Vincent <ritchie.vincent@propcom.co.uk>
2015-11-23 16:17:16 +0000

    Re-style buttons for geronimo

--- button.php
+++ button.php
@@ -4,7 +4,9 @@
        
         <? foreach($config as $button) :?>
         
-            <a href="<?= \Arr::get($button, 'link', '/');?>" class="button__button" title="<?= \Arr::get($button, 'label', 'Default');?>"><?= \Arr::get($button, 'label', 'Default');?></a>
+            <a href="<?= \Arr::get($button, 'link', '/');?>" class="button__button" title="<?= \Arr::get($button, 'label', 'Default');?>"><?= \Arr::get($button, 'label', 'Default');?>
+                <span class="button__button__dots"></span>
+            </a>
             
         <? endforeach; ?>
        
