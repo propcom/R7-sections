0c22a06b34a06421c750bc40856cb57e294d11b4
Samuel Woodbridge <s-bridge@live.co.uk>
2015-12-17 17:51:57 +0000

    [Youtube] - View updates

--- youtube.php
+++ youtube.php
@@ -1,8 +1,6 @@
 <div class="youtube">
     <div class="centre-wrap">        
-       
-        <? /* https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=50&playlistId=<?= \Arr::get($config, 'playlist');?>&key=<?= \Arr::get($config, 'authKey');?> */?>
-       
+          
         <div class="youtube__border">
             <h4><?= \Arr::get($config, 'title', 'Default Title');?></h4>
             <div class="youtube__wrapper">
@@ -10,16 +8,7 @@
             </div>
         </div>
         
-        <div class="youtube__buttons">
-            <a href="gqp8PBBgZHw" class="button__button active">Example</a>
-            <a href="fPkSzMyvbdM" class="button__button">Example</a>
-            <a href="cJBM7Zg4lYc" class="button__button">Example</a>
-            <a href="I1aRl-GKZOU" class="button__button">Example</a>
-            <a href="iACoI3Y6vOM" class="button__button">Example</a>
-            <a href="DZeVZ9gyq68" class="button__button">Example</a>
-            <a href="lWrljqfWbV0" class="button__button">Example</a>
-            <a href="L--T4LQViGw" class="button__button">Example</a>
-        </div>
+        <div class="youtube__buttons"></div>
         
     </div>
 </div>
