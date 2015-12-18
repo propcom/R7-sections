f86a2c9900268253a257ec67585d26ca77845f73
Samuel Woodbridge <s-bridge@live.co.uk>
2015-12-18 10:57:00 +0000

    [Youtube] - Small update

--- youtube.php
+++ youtube.php
@@ -1,11 +1,14 @@
 <div class="youtube">
     <div class="centre-wrap">        
           
-        <div class="youtube__border">
+        <div class="youtube__border clearfix">
             <h4><?= \Arr::get($config, 'title', 'Default Title');?></h4>
             <div class="youtube__wrapper">
                 <div id="player" class="youtube__player"></div>            
             </div>
+            <h4>
+                <a href="https://www.youtube.com/channel/UCBWWRLeA45NsHPI8LlWjY9Q" target="_blank">Click to see more</a>
+            </h4>
         </div>
         
         <div class="youtube__buttons"></div>
