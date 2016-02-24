69e1c1e4d1770963f6bce8901aed968e958682e5
Brett Chisholm <brett.chisholm@propcom.co.uk>
2016-02-24 15:36:59 +0000

    [Reskin] - make gallery fullscreen

--- gallery.php
+++ gallery.php
@@ -10,55 +10,73 @@
 ?>
 
 <section class="gallery">
+           
+    <h1 class="banner">
+        <?= Arr::get($config, 'title', 'Gallery');?>
+    </h1>
     
-    <div class="centre-wrap centre-wrap--centred">
-       
-    <h3 class="banner"><span><?= Arr::get($config, 'title', 'Gallery');?></span></h3>
-        
     <? $a = 0;?>
+    
     <? foreach($albums as $album) :?>
-    <div class="gallery__content" data-album="<?= $a;?>">
-        <? $images = $album->images();?>
-        <? $i = 0;?>
-        <? foreach($images as $image) :?>
-
-            <? if(($i == 1) || ($i == 7)): ?>
-                <div class="gallery__section">
-            <? endif; ?>
-
-
-            <div class="js-gallery-img  gallery__img--bg  gallery__img<?= $i == 5 ? ' gallery__img--large  gallery__img--large--right' : ''; ?> <?= $i == 6 ? ' gallery__img--large  gallery__img--large--left' : '';?>">
-                <span class="gallery__img--bg__img" style="background-image: url(<?= $image->get_src('medium');?>);"></span>
-            </div>
-
-            <? if(($i == 4) || ($i == 10)): ?>
-                </div>
-            <? endif; ?>
-
         
-        <? $i++;?>
-        <? endforeach;?>
-    </div>
-    <? $a++;?>
+        <div class="gallery__content" data-album="<?= $a;?>">
+            
+            <? $images = $album->images();?>
+        
+            <? $i = 0;?>
+        
+            <? foreach($images as $image) :?>
+
+                <? if(($i == 1) || ($i == 7)): ?>
+                    <div class="gallery__section">
+                <? endif; ?>
+
+
+                <div class="js-gallery-img  gallery__img--bg  gallery__img<?= $i == 5 ? ' gallery__img--large  gallery__img--large--right' : ''; ?> <?= $i == 6 ? ' gallery__img--large  gallery__img--large--left' : '';?>">
+                    <span class="gallery__img--bg__img" style="background-image: url(<?= $image->get_src('medium');?>);"></span>
+                </div>
+
+                <? if(($i == 4) || ($i == 10)): ?>
+                    </div>
+                <? endif; ?>
+
+            
+            <? $i++;?>
+            
+            <? endforeach;?>
+
+        </div>
+ 
+        <? $a++;?>
+
     <? endforeach;?>
         
-    </div>
     
     <div class="gallery__full">
-    <? foreach($albums as $album) :?>
-        <div class="gallery__slider">
-            <div class="inner-slider">
-            <? $images = $album->images();?>
-            <? foreach($images as $image) :?>
-                <img zRS-src="<?= $image->get_src('x-large');?>" alt="<?= $sitename;?>">
-            <? endforeach;?>            
-            </div>
+    
+        <? foreach($albums as $album) :?>
 
-            <a href="javascript:void(0);" class="gallery__nav prev">l</a>
-            <a href="javascript:void(0);" class="gallery__nav next">r</a>
-            
-        </div>    
-    <? endforeach;?>
+            <div class="gallery__slider">
+                
+                <div class="inner-slider">
+                   
+                    <? $images = $album->images();?>
+                    
+                    <? foreach($images as $image) :?>
+                        
+                        <img zRS-src="<?= $image->get_src('x-large');?>" alt="<?= $sitename;?>">
+
+                    <? endforeach;?>            
+
+                </div>
+
+                <a href="javascript:void(0);" class="gallery__nav prev">l</a>
+                <a href="javascript:void(0);" class="gallery__nav next">r</a>
+                
+            </div> 
+
+        <? endforeach;?>
+
     </div>
     
 </section>
