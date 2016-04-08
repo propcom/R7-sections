ae07834bc5904ea864f7797735f08761eb943f06
Ryan Clarke <ryan.clarke@propcom.co.uk>
2016-04-08 11:39:38 +0100

    [feautire-new-events] Merged all the events-new rules into the single events scss file.

--- events-new.php
+++ events-new.php
@@ -22,8 +22,8 @@
         
         <p class="larger"><?= \Arr::get($config, 'subtitle');?></p>
         
-        <div class="events__full" id="events-container">
-            <div class="events__months">
+        <div class="events__container" id="events-container">
+            <div class="events__months events__months--bold">
                 <ul>
                     <? for($m = 1; $m <= 12; $m++) :?>
                     <? $month = date('M', mktime(0, 0, 0, $m, 1, date('Y')));?>
@@ -37,18 +37,18 @@
                     	$date = strtotime($event->date_start);
                     	$pastDate = $date < time() ? 'events__entry__past' : '';  
                 ?>
-                    <div class="events__entry <?= $pastDate; ?>" id="<?= $event->id;?>">
+                    <div class="events__entry events__entry--narrow <?= $pastDate; ?>" id="<?= $event->id;?>">
                         <a href="/event?event=<?= $event->id;?>&date=<?= $event->date_start ?>">
 
                         <? if($event->image_id) :?>
-                            <div class="events__img">
+                            <div class="events__img events__img--wide">
                                 <div class="js-imager" data-src="<?= $event->image->get_src('large');?>" data-alt="<?= $event->name;?>"></div>
                                 <noscript>
                                     <img src="<?= $event->image->get_src('large');?>" alt="<?= $event->name;?>" height="185" width="265">                                
                                 </noscript>                                
                             </div>
                         <? else :?>
-                            <div class="events__img">
+                            <div class="events__img events__img--wide">
                                 <div class="js-imager" data-class="fit" data-src="/assets/img/regions/events/small-ph.png" data-alt="<?= $event->name;?>"></div>
                                 <noscript>
                                     <img src="/assets/img/regions/events/small-ph.png" class="fit" alt="<?= $event->name;?>" height="185" width="265">                                
@@ -56,12 +56,12 @@
                             </div>
                         <? endif;?>
 
-                        <div class="events__full-info">
-                            <div class="events__date" id="<?= date('d', $date);?>">
+                        <div class="events__full-info events__full-info--wide">
+                            <div class="events__date events__date--wide events__date--tertiary-color" id="<?= date('d', $date);?>">
                                 <span><?= date('F', $date);?></span>
                                 <p><?= date('d', $date);?></p>
                             </div>
-                            <div class="events__title">
+                            <div class="events__title events__title--narrow">
                                 <h5><?= $event->name;?></h5>
                                 <? if(strtotime($event->date_start) != strtotime($event->date_end)) :?>
                                     <p class="events__start"><?= date('g.iA', strtotime($event->date_start));?> - <?= date('g.iA', strtotime($event->date_end));?></p>                                    
@@ -69,7 +69,7 @@
                                     <p class="events__start"><?= date('g.iA', strtotime($event->date_start));?></p>                                
                                 <? endif;?>
                             </div>                        
-                            <div class="events__content">
+                            <div class="events__content events__content--wide">
                                 <?
                                     if (strlen($event->description) >= 151) {
                                         echo strip_tags (substr($event->description, 0, 150)). " ... " ;
@@ -83,10 +83,10 @@
                                 <? if($event->booking_link || $event->more_info_link) :?>
                                     <div class="events__buttons">
                                         <? if($event->booking_link) :?>
-                                            <a class="button__button" href="<?= $event->booking_link;?>" target="_blank">Book</a>                                        
+                                            <a class="button__button button__button--wide" href="<?= $event->booking_link;?>" target="_blank">Book</a>                                        
                                         <? endif;?>
                                         <? if($event->more_info_link) :?>
-                                            <a class="button__button" href="<?= $event->more_info_link;?>" target="_blank">Read More</a>                                        
+                                            <a class="button__button button__button--wide" href="<?= $event->more_info_link;?>" target="_blank">Read More</a>                                        
                                         <? endif;?>
                                     </div>
                                 <? endif;?>
