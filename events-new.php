37e3517184bc099c74976348a21f0839cb76937e
Josh Grierson <joshua.grierson@propcom.co.uk>
2016-04-19 14:36:54 +0100

    [112583] fix issue with not all events showing when using

--- events-new.php
+++ events-new.php
@@ -4,13 +4,17 @@
 	Shared_Autoloader::forge()->setVersion( 'v1' )->register();
 	$calendar_id = \Arr::get( $config, 'calendar' );
 	$calendar    = \Prop\CP\Events\Calendar::forge( $siteid, $calendar_id );
-	$get_month   = ( isset( $_GET['month'] ) ? date( 'M', strtotime( $_GET['month'], strtotime( 'first day of this month' ) ) ) : date( 'M' ) );
-	$event_month = ( isset( $_GET['month'] ) ? date( 'd M Y', strtotime( $_GET['month'], strtotime( 'first day of this month' ) ) ) : date( 'd M Y' ) );
-	$month_days = cal_days_in_month( CAL_GREGORIAN, ( date( 'n', strtotime( $event_month ) ) ), ( date( 'Y', strtotime( $event_month ) ) ) );
-	if ( time() > strtotime('+'.$month_days.'days', strtotime($event_month)) ) {
-		$event_month = date( 'M Y', strtotime( '+1 year', strtotime( $event_month ) ) );
-	}
-	$events = $calendar->get_events( $event_month, 'last day of this month' );
+
+    if(isset($_GET['month'])){
+        $current_month = date('m', strtotime($_GET['month']));
+    } else {
+        $current_month = date('m');
+    }
+
+    $events_first = $calendar->get_events('2016-'.$current_month.'-01', '2016-'.$current_month.'-14');
+    $events_second = $calendar->get_events('2016-'.$current_month.'-15', 'last day of this month');
+
+    $events = array_merge($events_first, $events_second);
 ?>
 <div class="events events--full">
 
@@ -27,80 +31,88 @@
                 <ul>
                     <? for($m = 1; $m <= 12; $m++) :?>
                     <? $month = date('M', mktime(0, 0, 0, $m, 1, date('Y')));?>
-                    <li><a href="/<?= \Arr::get($config, 'url', 'events');?>?month=<?= $month;?>#events-container"<?= $month === $get_month ? ' class="active"' : '';?>><?= $month;?> &rarr;</a></li>
+                    <li>
+                        <a href="/<?= \Arr::get($config, 'url', 'events');?>?month=<?= $month;?>#events-container"
+                        <? if(isset($_GET['month']) && date('m', strtotime($_GET['month'])) == date('m', strtotime($month))){ echo 'class="active";'; } ?>
+                        <? if(!isset($_GET['month']) && date('m') == date('m', strtotime($month))){ echo 'class="active";'; } ?>>
+                            <?= $month;?>&rarr;
+                        </a></li>
                     <? endfor; ?>
                 </ul>
             </div>
-        <? if($events) :?>
-            <? foreach($events as $date => $date_events):?>
-                <? foreach($date_events as $event):
-                    	$date = strtotime($event->date_start);
-                    	$pastDate = $date < time() ? 'events__entry--past' : '';
-                ?>
-                    <div class="events__entry events__entry--narrow <?= $pastDate; ?>" id="<?= $event->id;?>">
-                        <a href="/event?event=<?= $event->id;?>&date=<?= $event->date_start ?>">
+            
+            <? if($events) :?>
+                <? foreach($events as $date => $date_events):?>
+                    <? foreach($date_events as $event):
+                        
+                        $date = strtotime($event->date_start);
+                        $pastDate = $date < time() ? 'events__entry--past' : '';
+                        ?>
+                    
+                        <div class="events__entry events__entry--narrow <?= $pastDate; ?>" id="<?= $event->id;?>"  <? if($pastDate != '' && !isset($_GET['month'])){ echo 'style="display: none";'; } ?>>
+                            <a href="/event?event=<?= $event->id;?>&date=<?= $event->date_start ?>">
 
-                        <? if($event->image_id) :?>
-                            <div class="events__img events__img--wide">
-                                <div class="js-imager" data-src="<?= $event->image->get_src('large');?>" data-alt="<?= $event->name;?>"></div>
-                                <noscript>
-                                    <img src="<?= $event->image->get_src('large');?>" alt="<?= $event->name;?>" height="185" width="265">
-                                </noscript>
-                            </div>
-                        <? else :?>
-                            <div class="events__img events__img--wide">
-                                <div class="js-imager" data-class="fit" data-src="/assets/img/regions/events/small-ph.png" data-alt="<?= $event->name;?>"></div>
-                                <noscript>
-                                    <img src="/assets/img/regions/events/small-ph.png" class="fit" alt="<?= $event->name;?>" height="185" width="265">
-                                </noscript>
-                            </div>
-                        <? endif;?>
-
-                        <div class="events__full-info events__full-info--wide">
-                            <div class="events__date events__date--wide events__date--tertiary-color" id="<?= date('d', $date);?>">
-                                <span><?= date('F', $date);?></span>
-                                <p><?= date('d', $date);?></p>
-                            </div>
-                            <div class="events__title events__title--narrow">
-                                <h5><?= $event->name;?></h5>
-                                <? if(strtotime($event->date_start) != strtotime($event->date_end)) :?>
-                                    <p class="events__start"><?= date('g.iA', strtotime($event->date_start));?> - <?= date('g.iA', strtotime($event->date_end));?></p>
+                                <? if($event->image_id) :?>
+                                    <div class="events__img events__img--wide">
+                                        <div class="js-imager" data-src="<?= $event->image->get_src('large');?>" data-alt="<?= $event->name;?>"></div>
+                                        <noscript>
+                                            <img src="<?= $event->image->get_src('large');?>" alt="<?= $event->name;?>" height="185" width="265">
+                                        </noscript>
+                                    </div>
                                 <? else :?>
-                                    <p class="events__start"><?= date('g.iA', strtotime($event->date_start));?></p>
-                                <? endif;?>
-                            </div>
-                            <div class="events__content events__content--wide">
-                                <?
-                                    if (strlen($event->description) >= 151) {
-                                        echo strip_tags (substr($event->description, 0, 150)). " ... " ;
-                                    }
-                                    else {
-                                        echo strip_tags ($event->description);
-                                    }
-
-                                ?>
-
-                                <? if($event->booking_link || $event->more_info_link) :?>
-                                    <div class="events__buttons">
-                                        <? if($event->booking_link) :?>
-                                            <a class="button__button button__button--wide" href="<?= $event->booking_link;?>" target="_blank">Book</a>
-                                        <? endif;?>
-                                        <? if($event->more_info_link) :?>
-                                            <a class="button__button button__button--wide" href="<?= $event->more_info_link;?>" target="_blank">Read More</a>
-                                        <? endif;?>
+                                    <div class="events__img events__img--wide">
+                                        <div class="js-imager" data-class="fit" data-src="/assets/img/regions/events/small-ph.png" data-alt="<?= $event->name;?>"></div>
+                                        <noscript>
+                                            <img src="/assets/img/regions/events/small-ph.png" class="fit" alt="<?= $event->name;?>" height="185" width="265">
+                                        </noscript>
                                     </div>
                                 <? endif;?>
-                            </div>
+
+                                <div class="events__full-info events__full-info--wide">
+                                    <div class="events__date events__date--wide events__date--tertiary-color" id="<?= date('d', $date);?>">
+                                        <span><?= date('F', $date);?></span>
+                                        <p><?= date('d', $date);?></p>
+                                    </div>
+                                    <div class="events__title events__title--narrow">
+                                        <h5><?= $event->name;?></h5>
+                                        <? if(strtotime($event->date_start) != strtotime($event->date_end)) :?>
+                                            <p class="events__start"><?= date('g.iA', strtotime($event->date_start));?> - <?= date('g.iA', strtotime($event->date_end));?></p>
+                                        <? else :?>
+                                            <p class="events__start"><?= date('g.iA', strtotime($event->date_start));?></p>
+                                        <? endif;?>
+                                    </div>
+                                    <div class="events__content events__content--wide">
+                                        <?
+                                            if (strlen($event->description) >= 151) {
+                                                echo strip_tags (substr($event->description, 0, 150)). " ... " ;
+                                            }
+                                            else {
+                                                echo strip_tags ($event->description);
+                                            }
+
+                                        ?>
+
+                                        <? if($event->booking_link || $event->more_info_link) :?>
+                                            <div class="events__buttons">
+                                                <? if($event->booking_link) :?>
+                                                    <a class="button__button button__button--wide" href="<?= $event->booking_link;?>" target="_blank">Book</a>
+                                                <? endif;?>
+                                                <? if($event->more_info_link) :?>
+                                                    <a class="button__button button__button--wide" href="<?= $event->more_info_link;?>" target="_blank">Read More</a>
+                                                <? endif;?>
+                                            </div>
+                                        <? endif;?>
+                                    </div>
+                                </div>
+                            </a>
                         </div>
-                        </a>
-                    </div>
+                    <? endforeach;?>
                 <? endforeach;?>
-            <? endforeach;?>
-        <? else: ?>
+            <? else: ?>
 
-        <p>We currently don't have any events planned for <span class="highlight"><?= date('F Y', strtotime($event_month));?></span>, please check back soon for updates.</p>
+                <p>We currently don't have any events planned for <span class="highlight"><?= date('F Y', strtotime($event_month));?></span>, please check back soon for updates.</p>
 
-        <? endif;?>
+            <? endif;?>
         </div>
 
     </div>
