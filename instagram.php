a3efd2da3a46c57eabdef9c860ada6cb8ca87d26
Callam Williams <callam.williams@propcom.co.uk>
2016-07-11 16:51:46 +0100

    [bugfix] - instagram

--- instagram.php
+++ instagram.php
@@ -6,14 +6,15 @@ class Instagram {
 	public $count;
 	public $error = false;
 
-	public function __construct( $token, $count ) {
+	public function __construct( $user_id, $count ) {
 
+		$this->user_id = $user_id;
 		$this->count = $count;
-		$this->token = $token;
+		$this->token = '414143281.e2a9043.6d4acb839c38488f831d826bf29d32fe';
 
 		try {
 
-			$this->result = json_decode( $this->fetch( 'https://api.instagram.com/v1/users/self/media/recent?count=' . $this->count . '&access_token=' . $this->token ) );
+			$this->result = json_decode( $this->fetch( 'https://api.instagram.com/v1/users/'. $user_id .'/media/recent?count=' . $this->count . '&access_token=' . $this->token ) );
 
 			if ( isset( $this->result->meta->error_message ) ) {
 
@@ -65,7 +66,7 @@ class Instagram {
 
 }
 
-$insta = new Instagram( \Arr::get( $config, 'token' ), \Arr::get( $config, 'count', 30 ) );
+$insta = new Instagram( \Arr::get( $config, 'user_id' ), \Arr::get( $config, 'count', 30 ) );
 
 ?>
 
