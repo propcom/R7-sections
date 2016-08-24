af981e0c39775326e937ae43980d963c0b8c3666
Nicholas Carter <nicholas.carter@propcom.co.uk>
2016-08-24 14:37:08 +0100

    [Horrible] Ew whitespace fixes

--- cta.php
+++ cta.php
@@ -1,43 +1,34 @@
 <div class="cta">
-    
-    <div class="centre-wrap centre-wrap--centred">
-        
-        <? if(isset($config['regions']) && $config['regions']) :?>
-            
-                <? $i = 0;?>
-                <? foreach($config['regions'] as $region) :?>
+	<div class="centre-wrap centre-wrap--centred">
+		<? if (isset($config['regions']) && $config['regions']) :?>
+			<? $i = 0;?>
 
-                <div class="cta__region<?= ($i + 1) % 3 === 0 ? ' last' : '';?>">
+			<? foreach ($config['regions'] as $region) :?>
+				<div class="cta__region<?= ($i + 1) % 3 === 0 ? ' last' : '';?>">
+					<img src="<?= \Arr::get($region, 'image');?>" alt="<?= \Arr::get($region, 'title');?>" class="scale-with-grid">
 
-                    <img src="<?= \Arr::get($region, 'image');?>" alt="<?= \Arr::get($region, 'title');?>" class="scale-with-grid">
+					<h5><?= \Arr::get($region, 'title', 'Default');?></h5>
 
-                    <h5><?= \Arr::get($region, 'title', 'Default');?></h5>
-                    
-                    <?php $content = \Arr::get($region, 'content', 'Default content, please add your own.'); ?>
-                    <?php if(is_string($content)): ?>
-                        <p><?= \Str::truncate($content, 200, '...');?></p>
-                    <?php elseif(is_array($content)): ?>
-                        <?foreach ($content as $line): ?>
-                            <p><?= $line; ?></p>
-                        <?php endforeach; ?>
-                    <?php endif; ?>
+					<? $content = \Arr::get($region, 'content', 'Default content, please add your own.'); ?>
 
-                    <? if(isset($region['button'])) :?>
-                   
-                    <a href="<?= $region['button']['link'];?>" class="cta__button" title="<?= $region['button']['text'];?>"><?= $region['button']['text'];?></a>
+					<? if (is_string($content)): ?>
+						<p><?= \Str::truncate($content, 200, '...');?></p>
+					<? elseif (is_array($content)): ?>
+						<? foreach ($content as $line): ?>
+							<p><?= $line; ?></p>
+						<? endforeach; ?>
+					<? endif; ?>
 
-                    <? endif;?>
-               
-                </div>           
-                <? $i++;?>
-            <? endforeach;?>
-        
-        <? else: ?>
-      
-          <p>CTA: Please set up some regions in your config.</p>
-      
-        <? endif;?>  
-        
-    </div>
-    
+					<? if (isset($region['button'])) :?>
+						<a href="<?= $region['button']['link'];?>" class="cta__button" title="<?= $region['button']['text'];?>"><?= $region['button']['text'];?></a>
+					<? endif;?>
+				</div>
+
+				<? $i++;?>
+			<? endforeach;?>
+		<? else: ?>
+			<p>CTA: Please set up some regions in your config.</p>
+		<? endif;?>
+	</div>
+
 </div>
