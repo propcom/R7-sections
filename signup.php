ed35ccfd425626b25190abdca739eb76e05a0994
Ash Hancock <ashley.hancock@propcom.co.uk>
2018-02-21 14:48:50 +0000

    [feature-gdpr] - added markup and styles to contact form

--- signup.php
+++ signup.php
@@ -45,13 +45,13 @@ $email->setFromName('Signup from '.$sitename); // "Friendly" name emails are sen
 $email->addRecipient($site_email);
 
 if(isset($config['recipients']) && $config['recipients']) {
-    
+
     foreach($config['recipients'] as $recipient) {
-        
+
         $email->addRecipient($recipient);
-        
+
     }
-    
+
 }
 
 //$email->addRecipient('ruth.nachum@propcom.co.uk');		// Add a recipient to the email
@@ -76,12 +76,15 @@ $group3 = false;
 $group4 = false;
 $group5 = false;
 
+$fh->addField(new FormFieldBool('marketing-consent', false));
+$fh->addField(new FormFieldBool('profiling-consent', false));
+
 if(isset($config['listIDs']) && $config['listIDs']) {
-    
+
     foreach($config['listIDs'] as $listid) {
-        
+
         $listIDs[] = $listid;
-        
+
     }
 
 }
@@ -94,12 +97,12 @@ if(isset($config['listIDs']) && $config['listIDs']) {
 require '/var/www/shared/formincludes/signupFormFooter.php';
 ?>
 <div class="signup">
-    
-    <div class="signup__background">
-        
-        <div class="centre-wrap  centre-wrap--centred  centre-wrap--no  clearfix">
 
-            <div id="<?= $multiFormName; ?>-wrapper" class="signup__wrapper  clearfix">
+    <div class="signup__background">
+
+        <div class="centre-wrap  centre-wrap--centred  centre-wrap--no">
+
+            <div id="<?= $multiFormName; ?>-wrapper" class="signup__wrapper">
 
                 <? // Success Text  ?>
                 <? if ($fh->showSuccessText): ?>
@@ -119,57 +122,81 @@ require '/var/www/shared/formincludes/signupFormFooter.php';
                     <? else: ?>
 
                         <h4><?= \Arr::get($config, 'title', 'Claim your Free Drink');?></h4>
-                        <p class="welcomeText"><?= \Arr::get($config, 'subtitle', 'Join our mailing list and receive our news and events straight to your inbox.');?></p>
+                        <p class="welcomeText">If you would like to stay in touch, please confirm your communication preferences and review our <a href="http://www.youngs.co.uk/privacy-policy" target="_blank">privacy policy</a>.</p>
 
                     <? endif; ?>
 
                     <form action="" method="post" enctype="multipart/form-data" id="<?= $multiFormName; ?>" class="clearfix<?= $fh->showErrorText ? '  form-error' : ''?>">
-
-                        <div class="field-wrap<?php if ($fh->fields['forename']->isError) { ?> error<? } ?>">
-                            <label for="<?= $multiFormName ?>-forename">Name</label>
-                            <input type="text" name="forename" id="<?= $multiFormName ?>-forename" value="<?php echo $fh->fields['forename']->value ?>" <?= $forenameRequired ? 'required' : ''; ?> />
-                        </div>
-
-
-                        <div class="field-wrap<?php if ($fh->fields['email']->isError) { ?> error<? } ?>">
-                            <label for="<?= $multiFormName ?>-email">Email</label>
-                            <input type="email" name="email" id="<?= $multiFormName ?>-email" value="<?php echo $fh->fields['email']->value ?>" <?= $emailRequired ? 'required' : ''; ?> />
-                        </div>
-
-                        <div class="field-wrap<?php if ($fh->fields['dob']->isError) { ?> error<? } ?>">
-                            <label for="dob">Birthday</label>
-
-                            <div class="select-wrap">
-                                <select name="dob-day" id="dob-day" <?= $dobRequired ? 'required' : ''; ?>>
-                                    <option value="">DD</option>
-                                    <?php for ($i = 1; $i <= 31; $i++) { ?>
-                                        <option value="<?php echo $i ?>"<?php echo $fh->fields['dob']->day == $i ? ' selected="selected"' : '' ?>><?php echo $i ?></option>
-                                    <? } ?>
-                                </select>
-                                <div class="select">DD</div>
+                        <div class="clearfix">
+                            <div class="field-wrap<?php if ($fh->fields['forename']->isError) { ?> error<? } ?>">
+                                <label for="<?= $multiFormName ?>-forename">Name*</label>
+                                <input type="text" name="forename" id="<?= $multiFormName ?>-forename" value="<?php echo $fh->fields['forename']->value ?>" <?= $forenameRequired ? 'required' : ''; ?> />
                             </div>
 
-                            <div class="select-wrap">
-                                <select name="dob-month" id="dob-month" <?= $dobRequired ? 'required' : ''; ?>>
-                                    <option value="">MM</option>
-                                    <?php for ($i = 1; $i <= 12; $i++) { ?>
-                                        <option value="<?php echo $i ?>"<?php echo $fh->fields['dob']->month == $i ? ' selected="selected"' : '' ?>><?php echo date('M', mktime(0, 0, 0, $i, 1)) ?></option>
-                                    <? } ?>
-                                </select>
-                               <div class="select">MM</div>
-                           </div>    
+
+                            <div class="field-wrap<?php if ($fh->fields['email']->isError) { ?> error<? } ?>">
+                                <label for="<?= $multiFormName ?>-email">Email*</label>
+                                <input type="email" name="email" id="<?= $multiFormName ?>-email" value="<?php echo $fh->fields['email']->value ?>" <?= $emailRequired ? 'required' : ''; ?> />
+                            </div>
+
+                            <div class="field-wrap<?php if ($fh->fields['dob']->isError) { ?> error<? } ?>">
+                                <label for="dob">Birthday</label>
+
+                                <div class="select-wrap">
+                                    <select name="dob-day" id="dob-day" <?= $dobRequired ? 'required' : ''; ?>>
+                                        <option value="">DD</option>
+                                        <?php for ($i = 1; $i <= 31; $i++) { ?>
+                                            <option value="<?php echo $i ?>"<?php echo $fh->fields['dob']->day == $i ? ' selected="selected"' : '' ?>><?php echo $i ?></option>
+                                        <? } ?>
+                                    </select>
+                                    <div class="select">DD</div>
+                                </div>
+
+                                <div class="select-wrap">
+                                    <select name="dob-month" id="dob-month" <?= $dobRequired ? 'required' : ''; ?>>
+                                        <option value="">MM</option>
+                                        <?php for ($i = 1; $i <= 12; $i++) { ?>
+                                            <option value="<?php echo $i ?>"<?php echo $fh->fields['dob']->month == $i ? ' selected="selected"' : '' ?>><?php echo date('M', mktime(0, 0, 0, $i, 1)) ?></option>
+                                        <? } ?>
+                                    </select>
+                                   <div class="select">MM</div>
+                               </div>
+                            </div>
 
                         </div>
 
-                        <div style="display:none !important;">
-                            <textarea name="textboxfilter" rows="" cols=""></textarea>
-                            <input type="hidden" name="multiFormName" value="<?= $multiFormName ?>" />
+                        <div class="clearfix">
+                            <div class="consent">
+                                <div class="mb15<?php if (@$fh->fields['marketing-consent']->isError) { ?> error<? } ?>">
+                        			<input type="checkbox" name="marketing-consent" id="<?=$multiFormName?>-marketing-consent"
+                        				value=""<?php echo @$fh->fields['marketing-consent']->checked?'checked="checked" ':''?>
+                        				class="checkbox js-terms"
+                        			/>
+                        			<label for="<?=$multiFormName?>-marketing-consent">Sign me up for offers, news and promotions, mainly via email</label>
+                        		</div>
+
+                        		<div class="<?php if (@$fh->fields['profiling-consent']->isError) { ?> error<? } ?> js-field  profiling-consent">
+                        			<input type="checkbox" name="profiling-consent" id="<?=$multiFormName?>-profiling-consent"
+                        				value=""<?php echo @$fh->fields['profiling-consent']->checked?'checked="checked" ':''?>
+                        				class="checkbox "
+                        			/>
+                        			<label for="<?=$multiFormName?>-profiling-consent">I am happy for my data to be used to personalise my customer experience</label>
+                        		</div>
+                            </div>
                         </div>
 
-                        <input type="submit" name="submitted" value="Send" class="submit last" />
-                        
-                        <p class="terms"><a href="javascript:void(0);" class="js-terms">T &amp; Cs</a></p>
+                        <div class="clearfix">
+                            <div style="display:none !important;">
+                                <textarea name="textboxfilter" rows="" cols=""></textarea>
+                                <input type="hidden" name="multiFormName" value="<?= $multiFormName ?>" />
+                            </div>
 
+                            <input type="submit" name="submitted" value="Send" class="submit last" />
+                        </div>
+
+                        <p class="terms">
+                            Young & Co.’s Brewery, P.L.C and its group of companies are committed to protecting your data and it will be processed in accordance with our privacy policy which can be found at www.youngs.co.uk/privacy-policy. Please read this policy before signing up to our database. Promoter: Young & Co.’s Brewery P.L.C, Riverside House, 26 Osiers Road, Wandsworth, London SW18 1NH. Registered in England & Wales Company No. 32762.
+                        </p>
                     </form>
 
                 <? endif; ?>
@@ -177,27 +204,7 @@ require '/var/www/shared/formincludes/signupFormFooter.php';
             </div>
 
         </div>
-        
-    </div>    
-    
-</div>
 
-<div class="signup__terms">
-   
-    <p class="h_alpha">Terms &amp; Conditions</p>
-    
-    <? if($type === 'youngs') :?>
-    
-        <p>By filling in your details you are agreeing to hear from Young's and this pub from time to time. Your details will be stored in accordance with the Data Protection Act and we promise never to share your details with anyone else.</p>
-        <p>You may unsubscribe from Young's pubs at any time simply by clicking on the unsubscribe button at the bottom of the email or by emailing <a href="mailto:enquiries@youngs.co.uk?subject=Terms and Conditions Enquiry - <?= $sitename;?>">enquiries@youngs.co.uk</a>.</p>
-        <p>Promoter Young &amp; Co.'s Brewery, P.L.C. Riverside House, 26 Osiers Road, Wandsworth, London, SW18 1 NH. Registered Company No 32762</p>
-    
-    <? elseif($type === 'geronimo') :?>
-    
-        <p>By filling in your details you are agreeing to hear from Geronimo Inn’s and this pub from time to time. Your details will be stored in accordance with the Data Protection Act and we promise never to share your details with anyone else.</p>
-        <p>You may unsubscribe from Geronimo Inns pubs at any time simply by clicking on the unsubscribe button at the bottom of the email or by emailing <a href="mailto:enquiries@geronimo-inns.co.uk?subject=Terms and Conditions Enquiry - <?= $sitename;?>">enquiries@geronimo-inns.co.uk</a>.</p>
-        <p>Promoter Geronimo Inns, P.L.C. Riverside House, 26 Osiers Road, Wandsworth, London, SW18 1NH. Registered Company No 2979146</p>
-    
-    <? endif;?>
-    
+    </div>
+
 </div>
